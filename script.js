2c5c3a3daa9c5a7632f9aa82a1b713aacc6af5ed
SandroMercier <84975743+SandroMercier@users.noreply.github.com>
2022-02-02 14:20:27 -0800

    Add files via upload

---
+++ script.js
@@ -0,0 +1,6 @@
+let night = document.getElementById("night");
+const body = document.querySelector('body');
+
+night.addEventListener("click", () => {
+    document.body.classList.toggle("active")
+})
